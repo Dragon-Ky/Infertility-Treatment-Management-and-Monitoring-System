427b9ee32cbd12337f5827caf7b047dd981e89af
Dragon-Ky <giaky200601@gmail.com>
2026-04-01 18:35:02 +0700

    add: StorageRecord Model

---
+++ treatment-service/app/Models/StorageRecord.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class StorageRecord extends Model
+{
+    use HasFactory;
+
+    protected $table = 'storage_records';
+
+    protected $fillable = [
+        'treatment_id',
+        'storage_type',
+        'item_id',
+        'start_date',
+        'expiry_date',
+        'status',
+        'location_code',
+    ];
+
+    protected $casts = [
+        'start_date' => 'date',
+        'expiry_date' => 'date',
+    ];
+}
