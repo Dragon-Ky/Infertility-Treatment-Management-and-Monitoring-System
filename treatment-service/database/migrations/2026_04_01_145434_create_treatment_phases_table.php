498248cb99017c97cd7dd25f359eece161f66702
Dragon-Ky <giaky200601@gmail.com>
2026-04-01 22:47:01 +0700

    code : file treatment_phases data

--- treatment-service/database/migrations/2026_04_01_145434_create_treatment_phases_table.php
+++ treatment-service/database/migrations/2026_04_01_145434_create_treatment_phases_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('treatment_phases', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('protocol_id')->constrained('treatment_protocols')->onDelete('cascade');
+            // Khóa ngoại liên kết với bảng protocols. onDelete('cascade') nghĩa là xóa phác đồ thì xóa luôn giai đoạn này.
+            $table->string('phase_name'); // Tên giai đoạn (Ví dụ: Ovarian Stimulation)
+            $table->integer('order_index'); // Thứ tự của giai đoạn (1, 2, 3...)
+            $table->text('description')->nullable(); // Mô tả chi tiết mục tiêu giai đoạn
+            $table->enum('status', ['waiting', 'active', 'done'])->default('waiting'); // Trạng thái giai đoạn
             $table->timestamps();
         });
     }
