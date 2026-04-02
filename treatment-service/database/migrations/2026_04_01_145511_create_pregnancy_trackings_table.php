6cac65841cfb3274b1e8ed69db5868eb42e4cb87
Dragon-Ky <giaky200601@gmail.com>
2026-04-02 11:04:35 +0700

    code : file pregnancy_trackings data

--- treatment-service/database/migrations/2026_04_01_145511_create_pregnancy_trackings_table.php
+++ treatment-service/database/migrations/2026_04_01_145511_create_pregnancy_trackings_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('pregnancy_trackings', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('protocol_id')->constrained('treatment_protocols');
+            $table->decimal('beta_hcg_level', 8, 2)->nullable(); // Nồng độ Beta HCG
+            $table->integer('gestational_age_weeks')->nullable(); // Tuổi thai (tuần)
+            $table->string('fetal_heartbeat')->nullable(); // Tình trạng tim thai
+            $table->enum('outcome', ['ongoing', 'miscarriage', 'live_birth', 'ectopic']); // Kết quả thai kỳ
             $table->timestamps();
         });
     }
