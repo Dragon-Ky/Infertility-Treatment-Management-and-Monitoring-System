2a0028e8b0f836dba80125d47ecf96960d8d1048
Dragon-Ky <giaky200601@gmail.com>
2026-04-02 11:03:03 +0700

    code : file lab_results data

--- treatment-service/database/migrations/2026_04_01_145510_create_lab_results_table.php
+++ treatment-service/database/migrations/2026_04_01_145510_create_lab_results_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('lab_results', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('protocol_id')->constrained('treatment_protocols');
+            $table->string('test_name'); // Tên xét nghiệm (VD: AMH, LH, Estradiol)
+            $table->string('result_value'); // Giá trị kết quả
+            $table->string('unit'); // Đơn vị đo (VD: ng/ml, mIU/mL)
+            $table->text('interpretation')->nullable(); // Đánh giá của bác sĩ lab
             $table->timestamps();
         });
     }
