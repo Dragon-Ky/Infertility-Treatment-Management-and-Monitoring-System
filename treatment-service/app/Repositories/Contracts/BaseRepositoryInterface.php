b46c2d3a39d104551022ab428763ba729a34f108
Dragon-Ky <giaky200601@gmail.com>
2026-04-02 11:34:05 +0700

    create: add BaseRepositoryInterface for repository contract definition

---
+++ treatment-service/app/Repositories/Contracts/BaseRepositoryInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+interface BaseRepositoryInterface
+{
+    // Lấy tất cả bản ghi
+    public function all();
+
+    // Tìm một bản ghi theo ID
+    public function find($id);
+
+    // Tạo mới một bản ghi
+    public function create(array $attributes);
+
+    // Cập nhật bản ghi theo ID
+    public function update($id, array $attributes);
+
+    // Xóa bản ghi theo ID
+    public function delete($id);
+}
