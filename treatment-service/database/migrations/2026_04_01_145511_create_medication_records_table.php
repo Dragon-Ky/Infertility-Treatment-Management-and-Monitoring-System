10f36c0fca1b168d1ee2a7dabdfaba4c2271c3ca
Dragon-Ky <giaky200601@gmail.com>
2026-04-02 11:04:01 +0700

    code : file medication_records data

--- treatment-service/database/migrations/2026_04_01_145511_create_medication_records_table.php
+++ treatment-service/database/migrations/2026_04_01_145511_create_medication_records_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('medication_records', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('schedule_id')->constrained('medication_schedules'); // Liên kết với lịch đã lên
+            $table->dateTime('administered_at'); // Thời gian thực tế đã dùng thuốc
+            $table->unsignedBigInteger('staff_id'); // Người thực hiện (Y tá hoặc BN tự xác nhận)
+            $table->text('notes')->nullable(); // Phản ứng phụ hoặc ghi chú khác
             $table->timestamps();
         });
     }
