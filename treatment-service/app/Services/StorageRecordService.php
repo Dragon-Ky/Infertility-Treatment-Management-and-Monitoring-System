2e54d6e9d9d9be2bb66082713ce60b783fd862e1
Dragon-Ky <giaky200601@gmail.com>
2026-04-05 09:53:48 +0700

    feat : add updateStorage for StorageRecord updates

--- treatment-service/app/Services/StorageRecordService.php
+++ treatment-service/app/Services/StorageRecordService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\DTOs\Requests\CreateStorageRecordRequestDTO;
 use App\DTOs\Responses\StorageRecordResponseDTO;
+use App\DTOs\Requests\update\UpdateStorageRecordRequestDTO;
 use App\Repositories\Contracts\StorageRecordRepositoryInterface;
 use Illuminate\Support\Facades\DB;
 
@@ -27,4 +28,12 @@ class StorageRecordService
             return StorageRecordResponseDTO::fromModel($storage);
         });
     }
+    public function updateStorage(int $id, UpdateStorageRecordRequestDTO $dto): StorageRecordResponseDTO
+{
+    return DB::transaction(function () use ($id, $dto) {
+        $data = array_filter((array) $dto, fn($value) => !is_null($value));
+        $storage = $this->repository->update($id, $data);
+        return StorageRecordResponseDTO::fromModel($storage);
+    });
+}
 }
