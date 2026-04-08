0e23bf505e5c55b9e6954f5c9cf088b1dc80a0ea
MT <nguyenminhthai177@gmail.com>
2026-04-08 14:33:08 +0700

    implement service-category relationship and create initial service data

--- catalog-service/app/Models/Service.php
+++ catalog-service/app/Models/Service.php
@@ -20,4 +20,9 @@ class Service extends Model
     {
         return $this->belongsTo(ServiceCategory::class, 'service_category_id');
     }
+
+    public function pricings()
+    {
+        return $this->hasMany(ServicePricing::class);
+    }
 }
