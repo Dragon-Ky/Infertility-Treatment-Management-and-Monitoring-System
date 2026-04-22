b92439f95e24cec9301f9ea90f1771548cde3eb9
Boai <bttboai@gmail.com>
2026-04-22 20:46:47 +0700

    Restructure the frontend folder

--- frontend/src/services/doctorService.jsx
+++ frontend/src/services/doctorService.jsx
@@ -1,6 +1,6 @@
-import api from "../utils/auth-service/api";
+import api from "@/utils/auth-service/api";
 
 export const getPatientsForDoctor = async () => {
-  const response = await api.get("/admin/managers");
+  const response = await api.get("/doctor/customers");
   return response.data;
 };
--- frontend/src/services/managerService.jsx
+++ frontend/src/services/managerService.jsx
@@ -1,4 +1,4 @@
-import api from "../../utils/auth-service/api";
+import api from "@/utils/auth-service/api";
 
 export const getDoctors = async () => {
   const response = await api.get("/manager/doctors");
