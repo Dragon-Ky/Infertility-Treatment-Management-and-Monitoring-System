bbe0b6e167368d1c8f14c0e099a75b9e0d350ff0
Boai <bttboai@gmail.com>
2026-04-26 17:10:17 +0700

    Comment create user in treatment-service

--- treatment-service/database/seeders/DatabaseSeeder.php
+++ treatment-service/database/seeders/DatabaseSeeder.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Database\Seeders;
+// namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -17,9 +17,9 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
